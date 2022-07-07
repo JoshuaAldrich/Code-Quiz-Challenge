f29189c0d9c2410a2144226828f9d3e32225abfc
RengarLOL <joshuataldrich@gmail.com>
2022-07-07 18:58:06 -0400

    changes made to highscore js and css, start over button not linking to index.html

---
+++ Assets/JS/highscores.js
@@ -0,0 +1,27 @@
+var highScore = document.querySelector("#highScore");
+var clear = document.querySelector("#clear");
+var startOver = document.querySelector("startOver");
+
+// Clear Scores 
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+// Local Storage 
+var EveryScore = localStorage.getItem("EveryScore");
+EveryScore = JSON.parse(EveryScore);
+
+if (EveryScore !== null) {
+
+    for (var i = 0; i < EveryScore.length; i++) {
+
+        var createLi = document.createElement("li");
+        createLi.textContent = EveryScore[i].initials + " " + EveryScore[i].score;
+        highScore.appendChild(createLi);
+
+    }
+}
+// Go to index page BROKEN
+startOver.addEventListener("click", function () {
+    window.location.replace("index.html");
+});
