56aba9654ae14f2e0c440c24a931f0d182b16965
RengarLOL <joshuataldrich@gmail.com>
2022-07-06 21:00:21 -0400

    changes made to js file

---
+++ Assets/JS/script.js
@@ -0,0 +1,14 @@
+//need to create variables
+
+//Questions Objects
+
+//function to cycle objects questions and answers
+
+//start quiz to start timer
+
+//show scores and save high scores
+
+//clear high scores
+
+
+
