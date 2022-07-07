1a5882d62984e9bf1c5caec413b22a1aacb3d0a8
RengarLOL <joshuataldrich@gmail.com>
2022-07-06 21:44:20 -0400

    changes made to add real questions to JS

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -26,6 +26,66 @@ var score = 0;
 var 
 
 //Questions Objects
+var quizQuestions = [{
+    question: "Which of the following a common application used by programmers?",
+    choiceA: "AutoCAD",
+    choiceB: "Wrightsoft",
+    choiceC: "Blitz",
+    choiceD: "Visual Studios",
+    correctAnswer: "d"
+},
+
+{
+    question: "What does DOM stand for?",
+    choiceA: "Document Object Model",
+    choiceB: "Display Object Management",
+    choiceC: "Desktop Oriented Mode",
+    choiceD: "Data Object Manager",
+    correctAnswer: "a"
+},
+
+{
+    question: "How do you make comments in HTML?",
+    choiceA: "<!-- text -->",
+    choiceB: "/* text */",
+    choiceC: "// text //",
+    choiceD: "<text/>",
+    correctAnswer: "a"
+},
+
+{
+    question: "The condition in an if / else statement is enclose within _________.",
+    choiceA: "quotes",
+    choiceB: "curly brackets",
+    choiceC: "parentheses",
+    choiceD: "square brackets",
+    correctAnswer: "c",
+},
+{
+    question: "How do you call a function name myFunction?",
+    choiceA: "function = myFunction()",
+    choiceB: "createMyFunction()",
+    choiceC: "function:myFunction()",
+    choiceD: "function myFunction()",
+    correctAnswer: "d",
+},
+{
+    question: "The first index of an array is ______.",
+    choiceA: "0",
+    choiceB: "1",
+    choiceC: "null",
+    choiceD: "none of the above",
+    correctAnswer: "a",
+},
+{
+    question: "Javascript uses all but the following.",
+    choiceA: "strings",
+    choiceB: "numbers",
+    choiceC: "booleans", 
+    choiceD: "alerts",
+    correctAnswer: "d",
+},
+]
 
 //function to cycle objects questions and answers
 
