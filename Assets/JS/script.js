1982ef86515d5ba6bd47649a01cc40760d8b3ff2
RengarLOL <joshuataldrich@gmail.com>
2022-07-06 21:22:25 -0400

    trying to make global variables

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,4 +1,29 @@
 //need to create variables
+var quiz = document.getElementById("quiz");
+var resultsE1 = document.getElementById("result");
+var FinalScoreE1 = document.getElementById("finalScore");
+var gameOver = document.getElementById("gameOver");
+var questionsE1 = document.getElementById("questions");
+var quizTimer = document.getElementById("quizTimer");
+var startQuizbBtn = document.getElementById("start");
+var startQuizDiv = document.getElementById("startbtn");
+var highscoreContainer = document.getElementById("highscoreContainer");
+var highscoreDiv = document.getElementById("highscore");
+var highscoreInput = document.getElementById("quiz");
+var highscoreDisplay = document.getElementById("quiz");
+var endgameBtn = document.getElementById("quiz");
+var submitScoreBtn = document.getElementById("quiz");
+var highscoreDisplay = document.getElementById("quiz");
+var buttonA = document.getElementById("quiz");
+var buttonB = document.getElementById("quiz");
+var buttonC = document.getElementById("quiz");
+var buttonD = document.getElementById("quiz");
+var finalQuestionsIndex = document.getElementById("quiz");
+var currentQuestionsIndex = document.getElementById("quiz");
+var timeLeft = document.getElementById("quiz");
+var timeInterval = document.getElementById("quiz");
+var score = 0;
+var 
 
 //Questions Objects
 
