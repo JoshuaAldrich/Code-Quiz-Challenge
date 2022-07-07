8d28c8eb8480f11e44296571dc61afc043c5d96f
RengarLOL <joshuataldrich@gmail.com>
2022-07-07 18:09:11 -0400

    changes made to script.js to make questions variable easier strings to read

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,84 +1,33 @@
-//need to create variables
-
-var quizBody = document.getElementById("quiz");
-var resultsEl = document.getElementById("result");
-var finalScoreEl = document.getElementById("finalScore");
-var gameoverDiv = document.getElementById("gameover");
-var questionsEl = document.getElementById("questions");
-var quizTimer = document.getElementById("timer");
-var startQuizButton = document.getElementById("startbtn");
-var startQuizDiv = document.getElementById("startpage");
-var highscoreContainer = document.getElementById("highscoreContainer");
-var highscoreDiv = document.getElementById("high-scorePage");
-var highscoreInputName = document.getElementById("initials");
-var highscoreDisplayName = document.getElementById("highscore-initials");
-var endGameBtns = document.getElementById("endGameBtns");
-var submitScoreBtn = document.getElementById("submitScore");
-var highscoreDisplayScore = document.getElementById("highscore-score");
-var buttonA = document.getElementById("a");
-var buttonB = document.getElementById("b");
-var buttonC = document.getElementById("c");
-var buttonD = document.getElementById("d");
-
-
-//Questions Objects
-var quizQuestions = [{
+// Array of questions
+var quizQuestions = [
+    {
     question: "Which of the following a common application used by programmers?",
-    choiceA: "AutoCAD",
-    choiceB: "Wrightsoft",
-    choiceC: "Blitz",
-    choiceD: "Visual Studios",
-    correctAnswer: "d"
+    choices: ["AutoCAD","Wrightsoft","Blitz","Visual Studios"],
+    answer: "Visual Studios"
 },
-
 {
     question: "What does DOM stand for?",
-    choiceA: "Document Object Model",
-    choiceB: "Display Object Management",
-    choiceC: "Desktop Oriented Mode",
-    choiceD: "Data Object Manager",
-    correctAnswer: "a"
+    choices: ["Document Object Model","Display Object Management","Desktop Oriented Mode","Data Object Manager"],
+    answer: "Document Object Model"
 },
-
-{
-    question: "How do you make comments in HTML?",
-    choiceA: "<!-- text -->",
-    choiceB: "/* text */",
-    choiceC: "// text //",
-    choiceD: "<text/>",
-    correctAnswer: "a"
-},
-
 {
     question: "The condition in an if / else statement is enclose within _________.",
-    choiceA: "quotes",
-    choiceB: "curly brackets",
-    choiceC: "parentheses",
-    choiceD: "square brackets",
-    correctAnswer: "c",
+    choices: ["quotes","curly brackets","parentheses","square brackets"],
+    answer: "parentheses"
 },
 {
     question: "How do you call a function name myFunction?",
-    choiceA: "function = myFunction()",
-    choiceB: "createMyFunction()",
-    choiceC: "function:myFunction()",
-    choiceD: "function myFunction()",
-    correctAnswer: "d",
+    choices: ["function = myFunction()","createMyFunction()","function:myFunction()","function myFunction()"],
+    answer: "function myFunction()"
 },
 {
     question: "The first index of an array is ______.",
-    choiceA: "0",
-    choiceB: "1",
-    choiceC: "null",
-    choiceD: "none of the above",
-    correctAnswer: "a",
+    choices: ["0","1","null","none of the above"],
+    answer: "0"
 },
 {
     question: "Javascript uses all but the following.",
-    choiceA: "strings",
-    choiceB: "numbers",
-    choiceC: "booleans", 
-    choiceD: "alerts",
-    correctAnswer: "d",
+    choices: ["strings","numbers","booleans", "alerts"],
+    answer: "alerts"
 },
 ];
