7f7c6dff7e3400cf9742e20e4ee938b30170a955
RengarLOL <joshuataldrich@gmail.com>
2022-07-19 14:39:04 -0400

    changes to repo

---
+++ Assets/JS/highscores.js
@@ -0,0 +1,29 @@
+var highScore = document.querySelector("#highScore");
+var clear = document.querySelector("#clear");
+var startOver = document.querySelector("#startOver");
+// var inputEl = document.getElementById("input");
+// var initials = initialsEl.value.trim();
+
+// Clear Scores 
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+// Local Storage 
+var EveryScore = localStorage.getItem("EveryScore");
+EveryScore = JSON.parse(EveryScore);
+
+if (EveryScore !== null) {
+
+    for (var i = 0; i < EveryScore.length; i++) {
+
+        var createLi = document.createElement("li");
+        createLi.textContent = EveryScore[i].initials + " " + EveryScore[i].score;
+        highScore.appendChild(createLi);
+
+    }
+}
+// Go to index page
+startOver.addEventListener("click", function () {
+    window.location.replace("index.html");
+});
